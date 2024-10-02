5ad4a8d32f4d8485d4a1dd41f8ded882e6a3d4c9
dleel <david.lee5@toronto.ca>
2024-10-02 18:21:59 -0400

    changed param obj and return a function. checked undefined transitions

--- src/createModFSM.ts
+++ src/createModFSM.ts
@@ -1,41 +1,61 @@
 import { isInputValid } from './isInputValid';
 
 type State = string;
-type InputAlphabet = '0' | '1';
 
-type ModFSMFunction = (
-  states: Array<State>,
-  inputAlphabet: Array<string>,
-  initState: State,
-  finalStates: Array<State>,
-  transitions?: Record<State, Record<string, State>>
-) => { run: (inputStr: string) => number };
+interface ModFSMParams {
+  states: Array<State>;
+  inputAlphabet: Array<string>;
+  initState: State;
+  finalStates: Array<State>;
+  transitions?: Record<State, Record<string, State>>;
+  finalStateToOutput: { [index: string]: number };
+}
 
-export const createModFSM: ModFSMFunction = (
+type ModFSMReturn = (inputStr: string) => number;
+
+/**
+  A finite automaton (FA) is a 5-tuple (Q,Σ,q0,F,δ) where,
+
+  Q is a finite set of states;
+  Σ is a finite input alphabet;
+  q0 ∈ Q is the initial state;
+  F ⊆ Q is the set of accepting/final states; and
+  δ:Q×Σ→Q is the transition function.
+*/
+export const createModFSM = ({
   states,
   inputAlphabet,
   initState,
   finalStates,
-  transitions
-) => {
-  const remainder = 0;
-  const finalStateMap: { [index: string]: number } = { S0: 0, S1: 1, S2: 2 };
+  transitions,
+  finalStateToOutput,
+}: ModFSMParams): ModFSMReturn => {
+  // For any element q of Q and any symbol σ∈Σ, we interpret δ(q,σ) as the state to which the FA
+  // moves, if it is in state q and receives the input σ.
+  const run = (input: string) => {
+    let currentState = initState;
+    const symbols = input.split('');
 
-  const run = (inputString: string) => {
-    //validate inputAlphabet
-    // TODO: validate based on inputAlphabet. no need the util function
-    if (!isInputValid(inputString)) {
-      throw new Error(`input ${inputString} is not valid.`);
-    }
+    symbols.forEach((symbol) => {
+      if (!inputAlphabet.includes(symbol)) {
+        throw new Error(`Input symbol ${symbol} is not valid.`);
+      }
 
-    let nextState = initState;
-    const inputArr = inputString.split('');
+      // next state is current state + input symbol
+      const nextState = transitions![currentState]
+        ? transitions![currentState][symbol]
+        : null;
 
-    inputArr.forEach((input) => {
-      nextState = transitions![nextState][input];
+      if (!nextState) {
+        throw new Error(
+          `Transition is not defined for state ${currentState} and symbol ${symbol}`
+        );
+      }
+
+      currentState = nextState;
     });
 
-    return finalStateMap[nextState];
+    return finalStateToOutput[currentState];
   };
 
   // validate if initState is an element of states
@@ -43,6 +63,7 @@ export const createModFSM: ModFSMFunction = (
     throw new Error(`initState ${initState} is not an element of the states`);
   }
 
+  // F ⊆ Q is the set of accepting/final states;
   const invalidFinalState = finalStates.find(
     (finalState) => !states.includes(finalState)
   );
@@ -54,5 +75,5 @@ export const createModFSM: ModFSMFunction = (
     );
   }
 
-  return { run };
+  return run;
 };
--- tests/createModFSM.test.ts
+++ tests/createModFSM.test.ts
@@ -1,54 +1,221 @@
-import { expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { createModFSM } from '../src/createModFSM';
 
-const states = ['S0', 'S1', 'S3'];
 const inputAlphabet = ['0', '1'];
-const initState = 'S0';
-const finalState = ['S0', 'S1', 'S3'];
-const transitions = {
-  S0: { '0': 'S0', '1': 'S1' },
-  S1: { '0': 'S2', '1': 'S0' },
-  S2: { '0': 'S1', '1': 'S2' },
-};
 
-test('return an error with an invalid init state s4', () => {
-  expect(() =>
-    createModFSM(states, inputAlphabet, 'S4', finalState)
-  ).toThrowError(`initState S4 is not an element of the states`);
+describe('modulo three FSM', () => {
+  const states = ['S0', 'S1', 'S2'];
+  const initState = 'S0';
+  const finalStates = ['S0', 'S1', 'S2'];
+  const transitions = {
+    S0: { '0': 'S0', '1': 'S1' },
+    S1: { '0': 'S2', '1': 'S0' },
+    S2: { '0': 'S1', '1': 'S2' },
+  };
+  const finalStateToOutput = {
+    S0: 0,
+    S1: 1,
+    S2: 2,
+  };
+
+  test('return an error with an invalid init state S4', () => {
+    expect(() =>
+      createModFSM({
+        states,
+        inputAlphabet,
+        initState: 'S4',
+        finalStates,
+        finalStateToOutput,
+      })
+    ).toThrowError(`initState S4 is not an element of the states`);
+  });
+
+  test('return an error with an invalid final state ["S1", "S4"]', () => {
+    expect(() =>
+      createModFSM({
+        states,
+        inputAlphabet,
+        initState,
+        finalStates: ['S1', 'S5'],
+        finalStateToOutput,
+      })
+    ).toThrowError(`finalState S5 is not subset of the states`);
+  });
+
+  test('return an error with an invalid input string: "112"', () => {
+    const fsm = createModFSM({
+      states,
+      inputAlphabet,
+      initState,
+      finalStates,
+      transitions,
+      finalStateToOutput,
+    });
+
+    expect(() => fsm('112')).toThrowError('Input symbol 2 is not valid.');
+  });
+
+  test('undefined transition 110', () => {
+    const fsm = createModFSM({
+      states,
+      inputAlphabet,
+      initState,
+      finalStates,
+      transitions: {
+        S0: { '0': 'S0', '1': 'S1' },
+        S1: { '0': 'S2', '1': 'S0' },
+      },
+      finalStateToOutput,
+    });
+
+    expect(() => fsm('100')).toThrowError(
+      `Transition is not defined for state S2 and symbol 0`
+    );
+  });
+
+  test('return a remainder 0 : "110"', () => {
+    const fsm = createModFSM({
+      states,
+      inputAlphabet,
+      initState,
+      finalStates,
+      transitions,
+      finalStateToOutput,
+    });
+
+    expect(fsm('110')).toBe(0);
+  });
+
+  test('return a remainder 1 : "1010"', () => {
+    const fsm = createModFSM({
+      states,
+      inputAlphabet,
+      initState,
+      finalStates,
+      transitions,
+      finalStateToOutput,
+    });
+
+    expect(fsm('1010')).toBe(1);
+  });
+
+  test('return a remainder 2 : "1110"', () => {
+    const fsm = createModFSM({
+      states,
+      inputAlphabet,
+      initState,
+      finalStates,
+      transitions,
+      finalStateToOutput,
+    });
+
+    expect(fsm('1110')).toBe(2);
+  });
 });
 
-test('return an error with an invalid final state ["S1", "S4"]', () => {
-  expect(() =>
-    createModFSM(states, inputAlphabet, initState, ['S1', 'S5'])
-  ).toThrowError(`finalState S5 is not subset of the states`);
-});
+describe('modulo four FSM', () => {
+  const states = ['S0', 'S1', 'S2', 'S3'];
+  const initState = 'S0';
+  const finalStates = ['S0', 'S1', 'S2', 'S3'];
+  const transitions = {
+    S0: { '0': 'S0', '1': 'S1' },
+    S1: { '0': 'S2', '1': 'S3' },
+    S2: { '0': 'S0', '1': 'S1' },
+    S3: { '0': 'S2', '1': 'S3' },
+  };
+  const finalStateToOutput = {
+    S0: 0,
+    S1: 1,
+    S2: 2,
+    S3: 3,
+  };
 
-test('return an error with an invalid input string: "112"', () => {
-  const fsm = createModFSM(states, inputAlphabet, initState, finalState);
+  test('return an error with an invalid init state S4', () => {
+    expect(() =>
+      createModFSM({
+        states,
+        inputAlphabet,
+        initState: 'S4',
+        finalStates,
+        finalStateToOutput,
+      })
+    ).toThrowError(`initState S4 is not an element of the states`);
+  });
 
-  expect(() => fsm.run('112')).toThrowError('input 112 is not valid.');
-});
+  test('return an error with an invalid final state ["S1", "S4"]', () => {
+    expect(() =>
+      createModFSM({
+        states,
+        inputAlphabet,
+        initState,
+        finalStates: ['S1', 'S5'],
+        finalStateToOutput,
+      })
+    ).toThrowError(`finalState S5 is not subset of the states`);
+  });
 
-test('return a correct remainder 0 : "110"', () => {
-  const fsm = createModFSM(
-    states,
-    inputAlphabet,
-    initState,
-    finalState,
-    transitions
-  );
+  test('return an error with an invalid input string: "112"', () => {
+    const fsm = createModFSM({
+      states,
+      inputAlphabet,
+      initState,
+      finalStates,
+      transitions,
+      finalStateToOutput,
+    });
 
-  expect(fsm.run('110')).toBe(0);
-});
+    expect(() => fsm('112')).toThrowError('Input symbol 2 is not valid.');
+  });
 
-test('return a correct remainder 1 : "1010"', () => {
-  const fsm = createModFSM(
-    states,
-    inputAlphabet,
-    initState,
-    finalState,
-    transitions
-  );
+  test('return a remainder 0: "1100"', () => {
+    const fsm = createModFSM({
+      states,
+      inputAlphabet,
+      initState,
+      finalStates,
+      transitions,
+      finalStateToOutput,
+    });
 
-  expect(fsm.run('1010')).toBe(1);
+    expect(fsm('1100')).toBe(0);
+  });
+
+  test('return a remainder 1: "1101"', () => {
+    const fsm = createModFSM({
+      states,
+      inputAlphabet,
+      initState,
+      finalStates,
+      transitions,
+      finalStateToOutput,
+    });
+
+    expect(fsm('1101')).toBe(1);
+  });
+
+  test('return a remainder 2 : "1010"', () => {
+    const fsm = createModFSM({
+      states,
+      inputAlphabet,
+      initState,
+      finalStates,
+      transitions,
+      finalStateToOutput,
+    });
+
+    expect(fsm('1010')).toBe(2);
+  });
+
+  test('return a remainder 3 : "1011"', () => {
+    const fsm = createModFSM({
+      states,
+      inputAlphabet,
+      initState,
+      finalStates,
+      transitions,
+      finalStateToOutput,
+    });
+
+    expect(fsm('1011')).toBe(3);
+  });
 });
